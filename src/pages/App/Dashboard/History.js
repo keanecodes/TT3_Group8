b840b4a42b6df48db14f5e16f9815d8b8f272a05
Wayne <waynetanwh@gmail.com>
2021-03-05 13:28:21 +0800

    1327

--- src/pages/App/Dashboard/History.js
+++ src/pages/App/Dashboard/History.js
@@ -3,30 +3,27 @@ import axios from 'axios'
 
 export default function History() {
 
-    const [data, setData] = useState("")
+    const [history, setHistory] = useState("")
 
-    useEffect(() => {
-        axios.post(
-            'http://localhost:8080/https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/pricing/historical',
-            {
-                "x-api-key" : "4fLNtutUxi797l5cazMtm4z6FEEwCWm57NjjCvxP"
-            }
-        ).then((err,response) => {
-            if(err) {
-                setData("Error!")
-            } if (response) {
-                console.log(response)
-                setData(response)
-        
-            }
+    const fetchHistory = () => {
+        axios.post('http://localhost/8080/https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/pricing/historical', {},
+        {headers: {
+            'Content-Type': 'application/json;charset=UTF-8',
+            "X-Api-key": "4fLNtutUxi797l5cazMtm4z6FEEwCWm57NjjCvxP"}})
+        .then((response) => {
+            console.log(response.data);
+        }).catch((error) => {
+            // windows.alert(error);
+            console.log(error);
         })
-        
-    })
+    };
+
+
 
     return (
         <div>
             History Page
-            {data}
+            <button onClick ={fetchHistory}>View History</button>
         </div>
     )
 }
