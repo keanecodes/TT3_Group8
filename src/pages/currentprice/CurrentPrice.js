a09e12df5b42aa817afc95d2794b6220bcaeeafe
circledragon <57744342+circledragon@users.noreply.github.com>
2021-03-05 11:26:59 +0800

    add p tag with price

--- src/pages/currentprice/CurrentPrice.js
+++ src/pages/currentprice/CurrentPrice.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 const CurrentPrice = () => {
-
+    // this works put not sure how to pass the data out of function
     const getCurrentPrice = async () => {
         const res = await fetch("https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/pricing/current",
         {
@@ -12,12 +12,13 @@ const CurrentPrice = () => {
         }
         )
         const data = res.json()
-        console.log(data)
+        //console.log(data)
+        return data
     }
 
     return (
         <div>
-            
+            <p>{getCurrentPrice()['price']}</p>
         </div>
     )
 }
