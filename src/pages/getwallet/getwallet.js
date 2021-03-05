5aa2ec6a3640647b9e442bc2e83d9f440f9f67c3
akshaym961 <79921197+akshaym961@users.noreply.github.com>
2021-03-05 11:24:07 +0800

    Create getwallet.js

---
+++ src/pages/getwallet/getwallet.js
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+const CurrentWallet = () => {
+
+    const getCurrentWallet = async () => {
+        const res = await fetch("https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/balance",
+        {
+            method: "POST",
+            headers:{
+                'x-api-key': '4fLNtutUxi797l5cazMtm4z6FEEwCWm57NjjCvxP'
+            }
+        }
+        )
+        const data = res.json()
+        console.log(data)
+    }
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default CurrentWallet
